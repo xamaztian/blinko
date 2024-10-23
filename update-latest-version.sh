351baed1321cf8d88001db15bd7f6bd928e36669
blinko <blinkospace@gmail.com>
2024-10-23 21:32:23 +0800

    🍱chore: update sh

--- update-latest-version.sh
+++ update-latest-version.sh
@@ -8,6 +8,13 @@ DB_STATUS=$(docker compose -f docker-compose.prod.yml ps postgres | grep "Up")
 
 if [ -z "$DB_STATUS" ]; then
     echo "The database container is not running or exists and is starting a new database container..."
+
+    # 检查是否存在名为 blinko-postgres 的容器
+    if [ "$(docker ps -aq -f name=blinko-postgres)" ]; then
+        echo "A container with the name 'blinko-postgres' already exists. Removing it..."
+        docker rm -f blinko-postgres
+    fi
+
     docker compose -f docker-compose.prod.yml up -d postgres
 else
     echo "The database container is running, keeping the database container..."
