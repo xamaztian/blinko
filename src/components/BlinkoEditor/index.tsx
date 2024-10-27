21ef701a587f817b53e5a7fdab00d98bd10db9f6
blinko <blinkospace@gmail.com>
2024-10-27 20:33:06 +0800

    🐞fix: build issues

--- src/components/BlinkoEditor/index.tsx
+++ src/components/BlinkoEditor/index.tsx
@@ -18,15 +18,15 @@ export const BlinkoEditor = observer(({ mode, onSended, onHeightChange }: IProps
   return <div ref={editorRef} id='global-editor'>
     <Editor
       originFiles={!isCreateMode ? blinko.curSelectedNote?.attachments : []}
-      content={isCreateMode ? blinko.noteContent : blinko.curSelectedNote?.content}
+      content={isCreateMode ? blinko.noteContent! : blinko.curSelectedNote?.content!}
       onChange={v => {
         onHeightChange?.(editorRef.current.clientHeight)
-        isCreateMode ? (blinko.noteContent = v) : (blinko.curSelectedNote.content = v)
+        isCreateMode ? (blinko.noteContent = v) : (blinko.curSelectedNote!.content = v)
       }}
       isSendLoading={blinko.upsertNote.loading.value}
       bottomSlot={
         isCreateMode ? <div className='text-xs text-ignore ml-2'>Drop to upload files</div> :
-          <div className='text-xs text-desc'>{dayjs(blinko.curSelectedNote.createdAt).format("YYYY-MM-DD hh:mm:ss")}</div>
+          <div className='text-xs text-desc'>{dayjs(blinko.curSelectedNote!.createdAt).format("YYYY-MM-DD hh:mm:ss")}</div>
       }
       onSend={async ({ files }) => {
         if (isCreateMode) {
@@ -34,7 +34,7 @@ export const BlinkoEditor = observer(({ mode, onSended, onHeightChange }: IProps
           await blinko.upsertNote.call({ content: blinko.noteContent, attachments: files.map(i => { return { name: i.name, path: i.uploadPath, size: i.size } }) })
         } else {
           await blinko.upsertNote.call({
-            id: blinko.curSelectedNote.id,
+            id: blinko.curSelectedNote!.id,
             //@ts-ignore
             content: blinko.curSelectedNote.content, attachments: files.map(i => { return { name: i.name, path: i.uploadPath, size: i.size } })
           })
