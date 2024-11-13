d4650611f46cf273402f34643dd39813da70a48d
Blinko <blinkospace@gmail.com>
2024-11-13 20:40:53 +0800

    Update _app.ts

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -24,7 +24,7 @@ export const appRouter = router({
 });
 
 export const createCaller = t.createCallerFactory(appRouter);
-export const caller = createCaller({ id: '', name: '', sub: '', role: '', exp: 0, iat: 0 });
+export const caller = createCaller({ id: '1', name: 'admin', sub: '1', role: 'superadmin', exp: 0, iat: 0 });
 
 export type AppRouter = typeof appRouter;
 export type RouterOutput = inferRouterOutputs<AppRouter>;
