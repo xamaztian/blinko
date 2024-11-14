747a0df82a92b7d2439116797311a072262d1907
Blinko <blinko-space@gmail.com>
2024-11-14 13:54:18 +0800

    😘fix: pbkdf2 prefix

--- src/lib/serverHelper.ts
+++ src/lib/serverHelper.ts
@@ -5,7 +5,7 @@ export async function hashPassword(password: string): Promise<string> {
     const salt = crypto.randomBytes(16).toString('hex');
     crypto.pbkdf2(password, salt, 100000, 64, 'sha512', (err, derivedKey) => {
       if (err) reject(err);
-      resolve('pbkdf2:' + salt + ':' + derivedKey.toString('hex'));
+      resolve('$pbkdf2$:' + salt + ':' + derivedKey.toString('hex'));
     });
   });
 }
@@ -13,7 +13,7 @@ export async function hashPassword(password: string): Promise<string> {
 export async function verifyPassword(inputPassword: string, hashedPassword: string): Promise<boolean> {
   return new Promise((resolve, reject) => {
     const [prefix, salt, hash] = hashedPassword.split(':');
-    if (prefix !== 'pbkdf2') {
+    if (prefix !== '$pbkdf2$:') {
       return resolve(false);
     }
     crypto.pbkdf2(inputPassword, salt!, 100000, 64, 'sha512', (err, derivedKey) => {
