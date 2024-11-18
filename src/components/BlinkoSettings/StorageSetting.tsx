405d49331aa8b25d09788ee5bb6191519f5c7c15
Blinko <blinko-space@gmail.com>
2024-11-18 21:39:12 +0800

    fix: s3 cache

--- src/components/BlinkoSettings/StorageSetting.tsx
+++ src/components/BlinkoSettings/StorageSetting.tsx
@@ -1,19 +1,37 @@
 import { observer } from "mobx-react-lite";
-import { Button, Card, DropdownItem, DropdownMenu, DropdownTrigger, Dropdown, Input, Select, SelectItem, Switch, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react";
+import { Button, Card, DropdownItem, DropdownMenu, DropdownTrigger, Dropdown, Input } from "@nextui-org/react";
 import { RootStore } from "@/store";
 import { BlinkoStore } from "@/store/blinkoStore";
 import { PromiseCall } from "@/store/standard/PromiseState";
 import { Icon } from "@iconify/react";
-import { api, streamApi } from "@/lib/trpc";
+import { api } from "@/lib/trpc";
 import { Item } from "./Item";
 import { useTranslation } from "react-i18next";
 import { useMediaQuery } from "usehooks-ts";
+import { useEffect } from "react";
 
 
 export const StorageSetting = observer(() => {
   const isPc = useMediaQuery('(min-width: 768px)')
   const { t } = useTranslation()
   const blinko = RootStore.Get(BlinkoStore)
+  const store = RootStore.Local(() => ({
+    s3AccessKeyId: "",
+    s3AccessKeySecret: "",
+    s3Endpoint: "",
+    s3Region: "",
+    s3Bucket: "",
+  }))
+
+  useEffect(() => {
+    store.s3AccessKeyId = blinko.config.value?.s3AccessKeyId!
+    store.s3AccessKeySecret = blinko.config.value?.s3AccessKeySecret!
+    store.s3Endpoint = blinko.config.value?.s3Endpoint!
+    store.s3Region = blinko.config.value?.s3Region!
+    store.s3Bucket = blinko.config.value?.s3Bucket!
+  }, [blinko.config.value])
+
+
   return <Card shadow="none" className="flex flex-col p-4 bg-background">
     <div className='text-desc text-sm'>{t('storage')}</div>
     <Item
@@ -44,7 +62,7 @@ export const StorageSetting = observer(() => {
       blinko.config.value?.objectStorage === 's3' && <>
         <Item
           leftContent={<>Access key id</>}
-          rightContent={<Input value={blinko.config.value?.s3AccessKeyId} placeholder="Access key id" onBlur={async (e) => {
+          rightContent={<Input value={store.s3AccessKeyId} onChange={e => store.s3AccessKeyId = e.target.value} placeholder="Access key id" onBlur={async (e) => {
             await PromiseCall(api.config.update.mutate({
               key: 's3AccessKeyId',
               value: e.target.value
@@ -52,7 +70,7 @@ export const StorageSetting = observer(() => {
           }} />} />
         <Item
           leftContent={<>Access key secret</>}
-          rightContent={<Input value={blinko.config.value?.s3AccessKeySecret} placeholder="Access key secret" onBlur={async (e) => {
+          rightContent={<Input value={store.s3AccessKeySecret} onChange={e => store.s3AccessKeySecret = e.target.value} placeholder="Access key secret" onBlur={async (e) => {
             await PromiseCall(api.config.update.mutate({
               key: 's3AccessKeySecret',
               value: e.target.value
@@ -60,7 +78,7 @@ export const StorageSetting = observer(() => {
           }} />} />
         <Item
           leftContent={<>Endpoint</>}
-          rightContent={<Input value={blinko.config.value?.s3Endpoint} placeholder="Endpoint" onBlur={async (e) => {
+          rightContent={<Input value={store.s3Endpoint} onChange={e => store.s3Endpoint = e.target.value} placeholder="Endpoint" onBlur={async (e) => {
             await PromiseCall(api.config.update.mutate({
               key: 's3Endpoint',
               value: e.target.value
@@ -68,7 +86,7 @@ export const StorageSetting = observer(() => {
           }} />} />
         <Item
           leftContent={<>Region</>}
-          rightContent={<Input value={blinko.config.value?.s3Region} placeholder="Region" onBlur={async (e) => {
+          rightContent={<Input value={store.s3Region} onChange={e => store.s3Region = e.target.value} placeholder="Region" onBlur={async (e) => {
             await PromiseCall(api.config.update.mutate({
               key: 's3Region',
               value: e.target.value
@@ -76,7 +94,7 @@ export const StorageSetting = observer(() => {
           }} />} />
         <Item
           leftContent={<>Bucket</>}
-          rightContent={<Input value={blinko.config.value?.s3Bucket} placeholder="Bucket" onBlur={async (e) => {
+          rightContent={<Input value={store.s3Bucket} onChange={e => store.s3Bucket = e.target.value} placeholder="Bucket" onBlur={async (e) => {
             await PromiseCall(api.config.update.mutate({
               key: 's3Bucket',
               value: e.target.value
