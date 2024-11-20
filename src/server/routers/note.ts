52585d2440cf90cc0f51ecf70bf308bfcdf9878a
Blinko <blinko-space@gmail.com>
2024-11-20 15:08:16 +0800

    fix: escape character replace issue #143

--- src/server/routers/note.ts
+++ src/server/routers/note.ts
@@ -163,9 +163,9 @@ export const noteRouter = router({
     .mutation(async function ({ input, ctx }) {
       let { id, isArchived, type, attachments, content, isTop, isShare } = input
       if (content != null) {
-        content = content?.replace(/\\/g, '').replace(/&#x20;/g, ' ')
+        content = content?.replace(/&#x20;/g, ' ')
       }
-      const tagTree = helper.buildHashTagTreeFromHashString(extractHashtags(content + ' '))
+      const tagTree = helper.buildHashTagTreeFromHashString(extractHashtags(content?.replace(/\\/g, '') + ' '))
       let newTags: Prisma.tagCreateManyInput[] = []
       const handleAddTags = async (tagTree: TagTreeNode[], parentTag: Prisma.tagCreateManyInput | undefined, noteId?: number) => {
         for (const i of tagTree) {
