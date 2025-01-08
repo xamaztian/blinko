b2dbba8ce62a4040d8a1bcf527864ff9bf184e0a
blinko <blinkospace@gmail.com>
2025-01-08 13:56:00 +0800

    fix: move generateFeed function to helper module
    - Moved the generateFeed function from the atom route to the helper module for better code organization and reusability.
    - Updated the atom and rss routes to import the generateFeed function from the new location.
    - This change enhances maintainability and streamlines the feed generation process.

--- app/api/rss/[userId]/rss/route.ts
+++ app/api/rss/[userId]/rss/route.ts
@@ -1,5 +1,5 @@
+import { generateFeed } from '@/server/routers/helper';
 import { NextResponse } from 'next/server';
-import { generateFeed } from '../atom/route';
 
 export async function GET(
   request: Request,
