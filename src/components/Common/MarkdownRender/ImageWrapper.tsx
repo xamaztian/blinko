5c3c41c1ef3cbed318cd09702c75090dc8d27e8d
blinko <blinkospace@gmail.com>
2025-01-08 11:08:30 +0800

    fix: update image rendering styles and vditor CSS for better responsiveness
    - Adjusted the ImageWrapper component to use '!max-w-fit !m-auto' for improved image display.
    - Commented out the width property in vditor.css for images, replacing it with 'margin: 0 auto' to enhance centering and responsiveness.

--- src/components/Common/MarkdownRender/ImageWrapper.tsx
+++ src/components/Common/MarkdownRender/ImageWrapper.tsx
@@ -12,10 +12,10 @@ export const ImageWrapper = ({ src, width, height }: ImageWrapperProps) => {
   return (
     <div className='markdown-image-wrapper w-full'>
       <PhotoProvider>
-        <PhotoView src={src} width={1000}>
+        <PhotoView src={src} >
           <Image src={src} {...props}
             classNames={{
-              wrapper: '!max-w-full',
+              wrapper: '!max-w-fit !m-auto',
             }} className='w-full max-h-[200px] object-cover' />
         </PhotoView>
       </PhotoProvider>
