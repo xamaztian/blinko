285d9d57f885e2a39c7823aa540cbfcb8a2b03fc
blinko <blinkospace@gmail.com>
2025-03-11 09:26:52 +0800

    fix: build

--- app/api/rss/[userId]/atom/route.ts
+++ app/api/rss/[userId]/atom/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET(
   request: Request,
-  { params }: { params: { userId: string } }
+  { params }: { params: any }
 ) {
   const { searchParams } = new URL(request.url);
   
