c6d20394fb2be332301465a99c4dc41b107806f0
Jhao <jhao413@qq.com>
2025-03-30 15:35:15 +0800

    fix: Tooltip component display problems

--- src/components/BlinkoSettings/Item.tsx
+++ src/components/BlinkoSettings/Item.tsx
@@ -29,14 +29,14 @@ export const Item = observer(({ leftContent, rightContent, type = 'row', hidden 
 
 
 export const ItemWithTooltip = observer(({ content, toolTipContent }: { content: any, toolTipContent: any }) => {
-  return <div className="flex items-center gap-2">
-    {content}
-    <Tooltip content={<div className="max-w-[300px] flex flex-col gap-2 p-2">
+  return  <Tooltip content={<div className="max-w-[300px] flex flex-col gap-2 p-2">
       {toolTipContent}
     </div>}>
-      <Icon icon="proicons:info" width="18" height="18" />
+      <div className="flex items-center gap-2">
+        {content}
+        <Icon icon="proicons:info" width="18" height="18" />
+      </div>
     </Tooltip>
-  </div>
 })
 
 
